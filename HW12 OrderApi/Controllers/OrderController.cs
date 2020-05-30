d24f11662c67832bd0c178039744f60b2dea6c97
CJQ-CS-WHU <jiaqi.cs.whu@gmail.com>
2020-05-30 16:28:07 +0800

    修复了bug

---
+++ HW12 OrderApi/Controllers/OrderController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OrderApp;
+namespace OrderApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly OrderContext context;
+
+        //构造函数把TodoContext 作为参数，Asp.net core 框架可以自动注入TodoContext对象
+        public OrderController(OrderContext context)
+        {
+            this.context = context;
+        }
+        //订单号查询
+        // GET: api/order/{id}  id为路径参数
+        [HttpGet("{id}")]
+        public ActionResult<Order> GetOrder(int id)
+        {
+            var order
+            =context.Orders.SingleOrDefault(p => p.OrderID == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return order;
+        }
+        //通过客户名查询所有订单
+        // GET: api/todo
+        // GET: api/todo/pageQuery?customerName="陈家棋"
+        [HttpGet]
+        public ActionResult<List<Order>> queryOrderByCustomer(string customerName)
+        {
+            Customer customer = (Customer)context.Customers
+                     .Where(p => p.Name == customerName);
+                List<Order> query = context.Orders
+                    .Where(p => p.CustomerID == customer.ID)
+                    .ToList();
+            return query.ToList();
+        }
+
+        // POST: api/todo
+        [HttpPost]
+        public ActionResult<OrderItem> PostOrder(Order order)
+        {
+            try
+            {
+                context.Orders.Add(order);
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+            return NoContent();
+        }
+
+        // PUT: api/todo/{id}
+        [HttpPut("{id}")]
+        public ActionResult<OrderItem> UpdateOrder(Order newOrder)
+        {
+             try
+            {
+                Order oldOrder
+                    =context.Orders.SingleOrDefault(p => p.OrderID == newOrder.OrderID);
+                if (oldOrder == null)
+                    throw new Exception();
+                else 
+                {
+                    oldOrder = newOrder;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                if (e.InnerException != null) error = e.InnerException.Message;
+                return BadRequest(error);
+            }
+            return NoContent();
+        }
+
+        // DELETE: api/todo/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteOrder(int id)
+        {
+            try
+            {
+                var p = context.Orders
+                    .FirstOrDefault(x => x.OrderID == id);
+                context.Orders.Remove(p);
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+            return NoContent();
+        }
+
+    }
+}
---
+++ HW12 OrderApi/Models/Order.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace OrderApp
+{
+    public class Order : IComparable<Order>
+    {
+
+        [Key, Column(Order = 0)]
+        public int OrderID { get; set; }
+
+        public Customer Customer { get; set; }
+
+        [ForeignKey("Customer")]
+        public string CustomerID { get; set; }
+
+        [NotMapped]
+        public string CustomerName { get => (Customer != null) ? Customer.Name : ""; }
+
+        [Key,Column(Order = 1)]
+        public DateTime CreateTime { get; set; }
+
+        public List<OrderItem> items;
+
+        public Order() { items = new List<OrderItem>(); CreateTime = DateTime.Now; }
+
+        public Order(int orderId, Customer customer, List<OrderItem> items)
+        {
+            this.OrderID = orderId;
+            this.Customer = customer;
+            this.CustomerID = customer.ID;
+            this.CreateTime = DateTime.Now;
+            this.items = (items == null) ? new List<OrderItem>() : items;
+        }
+
+        public List<OrderItem> Items
+        {
+            get { return items; }
+        }
+
+        public double TotalPrice
+        {
+            get => items.Sum(item => item.TotalPrice);
+        }
+
+        //不做修改，数据库中会自动产生结果
+        public void AddItem(OrderItem orderItem)
+        {
+            if (Items.Contains(orderItem))
+                throw new ApplicationException($"添加错误：订单项{orderItem.GoodsName} 已经存在!");
+            Items.Add(orderItem);
+        }
+
+        //不做修改，数据库中会自动产生结果
+        public void RemoveItem(OrderItem orderItem)
+        {
+            Items.Remove(orderItem);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.Append($"Id:{OrderID}, customer:{Customer},orderTime:{CreateTime},totalPrice：{TotalPrice}");
+            items.ForEach(od => strBuilder.Append("\n\t" + od));
+            return strBuilder.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var order = obj as Order;
+            return order != null &&
+                   OrderID == order.OrderID;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -531220479;
+            hashCode = hashCode * -1521134295 + OrderID.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(CustomerName);
+            hashCode = hashCode * -1521134295 + CreateTime.GetHashCode();
+            return hashCode;
+        }
+
+        public int CompareTo(Order other)
+        {
+            if (other == null) return 1;
+            return this.OrderID.CompareTo(other.OrderID);
+        }
+    }
+}
